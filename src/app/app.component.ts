7bbad44a744811070c3a0eb4bdce2d861d43d133
Jhay1413 <93253483+Jhay1413@users.noreply.github.com>
2022-01-07 10:03:59 +0800

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calc';
+
+  public num1!:number;
+  public num2!:number;
+  public num3!:number;
+
+  add(){
+    this.num3=this.num1+this.num2;
+  }
+  div(){
+    this.num3=this.num1/this.num2;
+  }
+  sub(){
+    this.num3=this.num1-this.num2;
+  }
+  mult(){
+    this.num3=this.num1*this.num2;
+  }
 }
